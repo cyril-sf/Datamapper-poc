f21de88b43865f978be2986deb9a08bd2af5cd43
Cyril Fluck / Matt McGinnis <cyril@verticalresponse.com>
2011-09-23 15:40:25 -0700

    trying to make the migration work

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require 'data_mapper'
+
+# A MySQL connection:
+DataMapper.setup(:default, 'mysql://localhost/datamapper_development')
 
 # Initialize the rails application
 Datamapper::Application.initialize!
 
 # If you want the logs displayed you have to do this before the call to setup
 DataMapper::Logger.new($stdout, :debug)
-
-# A MySQL connection:
-DataMapper.setup(:default, 'mysql://localhost/datamapper_development')
-
-require 'data_mapper'
