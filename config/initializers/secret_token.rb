b7e5b7930ae75f448354358a4c01640796f39ede
VR Engineering Pair <pair@host-137-6-0-10.sfoffice.verticalresponse.com>
2011-09-23 10:58:59 -0700

    creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datamapper::Application.config.secret_token = 'f269e395f75e44bb41b466c875d7f8fa2d44f252e75ecd50e635d0c71a1ea272bfae15052a5b7e24b20c0f1c13d41646481f70c967500fd8984bb975722cf660'
